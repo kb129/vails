0e0a732ad30d8f044610d61dc7cd01e53394c0ba
kb <okod129@gmail.com>
2021-01-30 03:45:14 +0900

    fix code indent

--- vails/__init__.py
+++ vails/__init__.py
@@ -11,21 +11,30 @@ class vails:
     def parse(self):
         # add help for vails command
         parser = argparse.ArgumentParser(
-            prog='vails', description='This script is ...')
+            prog='vails',
+            description='This script is ...')
         subparsers = parser.add_subparsers(metavar='command')
 
         # add help for 'vails new' command
         parser_new = subparsers.add_parser(
-            'new', help='See `new -h`.',  aliases=['n'])
+            'new',
+            help='See `new -h`.',
+            aliases=['n'])
         parser_new.add_argument(
-            'project_name', type=str, help='project name what you want to create.')
+            'project_name',
+            type=str,
+            help='project name what you want to create.')
         parser_new.set_defaults(handler=self.new)
 
         # add help for 'vails generate' command
         parser_generate = subparsers.add_parser(
-            'generate', help='See `generate -h`.', aliases=['g'])
+            'generate',
+            help='See `generate -h`.',
+            aliases=['g'])
         parser_generate.add_argument(
-            'file_name', type=str, help='File name what you want to create.')
+            'file_name',
+            type=str,
+            help='File name what you want to create.')
         parser_generate.set_defaults(handler=self.generate)
         self.args = parser.parse_args()
 
